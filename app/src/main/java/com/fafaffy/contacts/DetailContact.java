6ee16a8a171c621891e389de90ec831a4a4eb94a
dochollywood <bjg150030@utdallas.edu>
2018-02-16 20:19:40 -0600

    Add Theme Colors/Gradients

--- app/src/main/java/com/fafaffy/contacts/DetailContact.java
+++ app/src/main/java/com/fafaffy/contacts/DetailContact.java
@@ -5,6 +5,7 @@ package com.fafaffy.contacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 /*
     This activity shows a contact's detail.
@@ -15,6 +16,10 @@ public class DetailContact extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_detail_contact);
+
     }
 }
